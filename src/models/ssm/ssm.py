b9f3425b6d85cf3b9799ee5623a52cf1cb439ef1
Giuseppe <g.lombardi11@studenti.unipi.it>
2024-03-01 19:58:09 +0100

    update ssm

--- src/models/ssm/ssm.py
+++ src/models/ssm/ssm.py
@@ -8,28 +8,50 @@ Define a model subclass of torch.nn.Module that implements a linear time-invaria
 
 
 class SSM(torch.nn.Module):
-    def __init__(self, d_input, eigs, delta_min, delta_max):
+    def __init__(self, d_input, eigs, delta_min, delta_max, dynamics='continuous', field='complex'):
         # x_new = Lambda_bar * x_old + B_bar * u_new
         # y_new = C * x_new + D * u_new
 
         super(SSM, self).__init__()
 
-        #self.Lambda = Lambda
-        self.Lambda = SSM.__state_matrix(eigs)
+        if field == 'real':
+            Lambda = SSM.__continuous_state_matrix(eigs)
+        elif field == 'complex':
+            Lambda = SSM.__continuous_complex_state_matrix(eigs)
+        else:
+            raise NotImplementedError
+
+        #self.Lambda = Lambda.clone().detach().requires_grad_(False)
+        Lambda = Lambda.clone().detach().requires_grad_(False)
 
         self.d_input = d_input
-        self.d_state = self.Lambda.shape[0]
+        self.d_state = Lambda.shape[0]
         self.d_output = self.d_input
 
-        self.B = torch.randn(self.d_state, self.d_input, dtype=torch.complex64)
-        self.C = torch.randn(self.d_output, self.d_state, dtype=torch.complex64)
-        self.D = torch.randn(self.d_output, self.d_input, dtype=torch.float32)
+        #B = torch.randn(self.d_state, self.d_input, dtype=torch.complex64)
+        #self.B = B.clone().detach().requires_grad_(False)
 
-        self.Delta = delta_min + (delta_max - delta_min) * torch.rand(self.d_state, 1, dtype=torch.float32)
-        self.Lambda_bar = self.__state_matrix(eigs)
-        self.Lambda_bar, self.B_bar = self.__zoh()  # Discretize once during initialization
+        C = torch.randn(self.d_output, self.d_state, dtype=torch.complex64)
+        self.C = nn.Parameter(C, requires_grad=True)
 
-        self.x = torch.zeros(self.d_state, 1, dtype=torch.float32)
+        D = torch.randn(self.d_output, self.d_input, dtype=torch.float32)
+        self.D = nn.Parameter(D, requires_grad=True)
+
+        #Delta = delta_min + (delta_max - delta_min) * torch.rand(self.d_state, 1, dtype=torch.float32)
+        #self.Delta = nn.Parameter(Delta, requires_grad=True)
+
+        if dynamics == 'continuous':
+            Delta = delta_min + (delta_max - delta_min) * torch.rand(self.d_state, 1, dtype=torch.float32)
+            B = torch.randn(self.d_state, self.d_input, dtype=torch.complex64)
+            Lambda_bar, B_bar = self.__zoh(Lambda, B, Delta)  # Discretize once during initialization
+        elif dynamics == 'discrete':
+            Lambda_bar = Lambda
+            B_bar = torch.randn(self.d_state, self.d_input, dtype=torch.complex64)
+        else:
+            raise NotImplementedError
+
+        self.Lambda_bar = Lambda_bar.clone().detach().requires_grad_(False)
+        self.B_bar = nn.Parameter(B_bar, requires_grad=True)
 
         self.output_linear = nn.Sequential(
             nn.GELU(),
@@ -37,9 +59,10 @@ class SSM(torch.nn.Module):
 
 
     @staticmethod
-    def __state_matrix(eigs):
+    def __continuous_state_matrix(eigs):
         """
-        Compute the state matrix of the system.
+        Compute the complex state matrix A of the continuous SSM
+        (similar to a real matrix: lambda -> conjugated lambda).
         """
         d_state = sum(mul * 2 for mul in eigs.values())
         Lambda = torch.empty(d_state, dtype=torch.complex64)
@@ -60,7 +83,46 @@ class SSM(torch.nn.Module):
         Lambda = Lambda.view(-1, 1)
         return Lambda
 
-    def __zoh(self):
+    @staticmethod
+    def __continuous_complex_state_matrix(eigs):
+        """
+        Compute the complex state matrix A of the continuous SSM.
+        """
+        d_state = sum(mul for mul in eigs.values())
+        Lambda = torch.empty(d_state, dtype=torch.complex64)
+
+        index = 0
+        for (alpha, omega), mul in eigs.items():
+            # Create tensors for the real and imaginary parts
+            alpha_tensor = torch.full((mul,), alpha, dtype=torch.float32)
+            omega_tensor = torch.full((mul,), omega, dtype=torch.float32)
+
+            # Construct the complex numbers
+            Lambda[index:index + mul] = torch.complex(alpha_tensor, omega_tensor)
+
+            index += mul
+
+        # Reshape Lambda to match the expected output shape
+        Lambda = Lambda.view(-1, 1)
+        return Lambda
+
+
+    # def __zoh(self):
+    #     """
+    #     Discretize the system using the zero-order-hold transform.
+    #     """
+    #     # A_bar = exp(A*Delta)
+    #     # B = (A_bar - I) * inv(A) * B
+    #     # C = C
+    #     # D = D
+    #     Ones = torch.ones(self.d_state, 1, dtype=torch.float32)
+    #     Lambda_bar = torch.exp(torch.mul(self.Lambda, self.Delta))
+    #     B_bar = torch.mul(torch.mul(1 / self.Lambda, (Lambda_bar - Ones)), self.B)
+    #
+    #     return Lambda_bar, B_bar
+
+    @staticmethod
+    def __zoh(Lambda, B, Delta):
         """
         Discretize the system using the zero-order-hold transform.
         """
@@ -68,9 +130,10 @@ class SSM(torch.nn.Module):
         # B = (A_bar - I) * inv(A) * B
         # C = C
         # D = D
-        Ones = torch.ones(self.d_state, 1, dtype=torch.float32)
-        Lambda_bar = torch.exp(torch.mul(self.Lambda, self.Delta))
-        B_bar = torch.mul(torch.mul(1 / self.Lambda, (Lambda_bar - Ones)), self.B)
+        Ones = torch.ones(Lambda.shape[0], 1, dtype=torch.float32)
+
+        Lambda_bar = torch.exp(torch.mul(Lambda, Delta))
+        B_bar = torch.mul(torch.mul(1 / Lambda, (Lambda_bar - Ones)), B)
 
         return Lambda_bar, B_bar
 
@@ -129,4 +192,10 @@ class SSM(torch.nn.Module):
         return self.output_linear(torch.mm(self.C, xs).real + Du)
 
     def forward(self, u):
-        return torch.vmap(self.__apply_ssm)(u)
+        # To update Lambda_bar (considering Delta trainable) FORSE E' UNA CAZZATA
+        #Lambda_bar, B_bar = self.__zoh()
+        #self.Lambda_bar = Lambda_bar.clone().detach().requires_grad_(False)
+        #self.B_bar = nn.Parameter(B_bar, requires_grad=True)
+
+        # Return a dummy state to satisfy this repo's interface, but this can be modified
+        return torch.vmap(self.__apply_ssm)(u), None
