a471afa7d538edf64c7c4975246eebc52e760b8f
icezimmer <g.lombardi11@studenti.unipi.it>
2024-03-18 00:57:22 +0100

    new stacked

--- main/run_smnist.py
+++ main/run_smnist.py
@@ -16,13 +16,13 @@ from src.ml.evaluation import EvaluateClassifier
 
 
 class TestSequentialMNIST:
-    def __init__(self, model_name, n_layers):
+    def __init__(self, model_name, n_layers, *args, **kwargs):
         self.__NUM_CLASSES = 10
         self.__CRITERION = torch.nn.CrossEntropyLoss()  # Classification task: sigmoid layer + BCE loss (more stable)
-        self.model = self.__construct_model(model_name, n_layers)
+        self.model = self.__construct_model(model_name, n_layers, *args, **kwargs)
 
-    def __construct_model(self, block, n_layers):
-        stacked = NaiveStacked(block=block, n_layers=n_layers)
+    def __construct_model(self, block_factory, n_layers, *args, **kwargs):
+        stacked = NaiveStacked(block_factory=block_factory, n_layers=n_layers, *args, **kwargs)
         classifier = Seq2Vec(model=stacked, d_vec=self.__NUM_CLASSES)
 
         return classifier
@@ -44,10 +44,9 @@ if __name__ == "__main__":
     train_dataloader = load_temp_data('smnist_train_dataloader')
     test_dataloader = load_temp_data('smnist_test_dataloader')
 
-    block = S4D(d_input=NUM_FEATURES_INPUT, d_state=2048)
-    smnist = TestSequentialMNIST(block, n_layers=1)
+    smnist = TestSequentialMNIST(S4D, n_layers=4, d_input=NUM_FEATURES_INPUT, d_state=2048)
 
-    smnist.fit_model(num_epochs=50, lr=0.001, train_dataloader=train_dataloader, device_name='cuda:1')
+    smnist.fit_model(num_epochs=200, lr=0.001, train_dataloader=train_dataloader, device_name='cuda:1')
 
     smnist.evaluate_model(train_dataloader, 'cuda:1')
     smnist.evaluate_model(test_dataloader, 'cuda:1')
--- src/models/nn/stacked.py
+++ src/models/nn/stacked.py
@@ -2,10 +2,15 @@ import torch.nn as nn
 
 
 class NaiveStacked(nn.Module):
-    def __init__(self, block, n_layers):
+    def __init__(self, block_factory, n_layers, *args, **kwargs):
         super(NaiveStacked, self).__init__()
-        self.d_output = block.d_output
-        self.layers = nn.ModuleList([block for _ in range(n_layers)])
+        self.layers = nn.ModuleList([block_factory(*args, **kwargs) for _ in range(n_layers)])
+
+        # Assuming the last block's output dimension is representative for the whole stack
+        if n_layers > 0:
+            self.d_output = self.layers[-1].d_output
+        else:
+            self.d_output = None
 
     def forward(self, x):
         for layer in self.layers:
