61d3fbbc86c6f0ec7c369515359525e48f412b50
icezimmer <g.lombardi11@studenti.unipi.it>
2024-05-05 18:16:36 +0200

    register buffer for esn

--- src/models/esn/esn.py
+++ src/models/esn/esn.py
@@ -7,7 +7,7 @@ from src.reservoir.matrices import Reservoir
 class ESN(nn.Module):
     """Class of Echo State Network model using PyTorch."""
 
-    def __init__(self, d_input, d_state, input_scaling=1.0, spectral_radius=1.0, leakage_rate=0.5):
+    def __init__(self, d_input, d_state, input_scaling, spectral_radius, leakage_rate):
         """
         Constructor of ESN model.
         """
@@ -18,40 +18,33 @@ class ESN(nn.Module):
 
         input2state_reservoir = Reservoir(d_in=self.d_input, d_out=self.d_state)
         w_in = input2state_reservoir.uniform_disk_matrix(radius=input_scaling, field='real')
-        self.w_in = nn.Parameter(w_in, requires_grad=False)
+        self.register_buffer('w_in', w_in)
 
         self.register_buffer('x0', torch.zeros(self.d_state, dtype=torch.float32))
 
         state_reservoir = DiscreteStateReservoir(self.d_state)
         w_hh = state_reservoir.echo_state_matrix(max_radius=spectral_radius)
-        self.w_hh = nn.Parameter(w_hh, requires_grad=False)
+        self.register_buffer('w_hh', w_hh)
 
-        self.bias = nn.Parameter(torch.rand(self.d_state) * 2 - 1, requires_grad=False)
+        self.register_buffer('bias', torch.rand(self.d_state, dtype=torch.float32) * 2 - 1)
         self.nl = nn.Tanh()
 
     def step(self, u, x=None):
+        # Step function for updating state
         if x is None:
-            x = self.x0.unsqueeze(0).expand(u.shape[0], -1)
-        preactivation = (torch.einsum('ph, bh -> bp', self.w_in, u)
-                         + torch.einsum('pp, bp -> bp', self.w_hh, x)
-                         + self.bias.squeeze(0))
+            x = self.x0.unsqueeze(0).expand_as(u)
+        preactivation = (torch.einsum('ph, bh -> bp', self.w_in, u) +
+                         torch.einsum('pp, bp -> bp', self.w_hh, x) +
+                         self.bias)
         x = (1 - self.leakage_rate) * x + self.leakage_rate * self.nl(preactivation)
         return x
 
-    def forward(self, u: torch.Tensor):
-        """
-        Forward pass of ESN model.
-        """
+    def forward(self, u):
+        # Forward pass for processing sequences
         x_prev = self.x0.unsqueeze(0).expand(u.shape[0], -1)
         x_list = []
         for k in range(u.shape[-1]):
-            u_k = u[:, :, k]
-            preactivation = (torch.einsum('ph, bh -> bp', self.w_in, u_k)
-                             + torch.einsum('pp, bp -> bp', self.w_hh, x_prev)
-                             + self.bias.squeeze(0))
-            x_curr = (1 - self.leakage_rate) * x_prev + self.leakage_rate * self.nl(preactivation)
+            x_curr = self.step(u[:, :, k], x_prev)
             x_list.append(x_curr.unsqueeze(-1))
             x_prev = x_curr
-        x = torch.cat(tensors=x_list, dim=-1)
-
-        return x, None
+        return torch.cat(x_list, dim=-1), None
