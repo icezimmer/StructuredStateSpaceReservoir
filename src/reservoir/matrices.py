2e712b30f33ff27f9de7ba2100248446e156a244
icezimmer <g.lombardi11@studenti.unipi.it>
2024-05-07 19:14:42 +0200

    refactor reservoir matrices

--- src/reservoir/matrices.py
+++ src/reservoir/matrices.py
@@ -2,15 +2,15 @@ import warnings
 import torch
 
 
-class Reservoir:
+class ReservoirMatrix:
     def __init__(self, d_in, d_out):
         """
-        Generate the discrete state matrix.
+        Generate random matrices.
         """
         self.d_in = d_in
         self.d_out = d_out
 
-    def uniform_disk_matrix(self, radius, field):
+    def uniform_disk(self, radius, field):
         """
         Create the random uniform matrix such that each matrix values: -radius <= |W_ij| < radius.
         :param radius: float, the radius of the matrix
@@ -34,7 +34,7 @@ class Reservoir:
 
         return W
 
-    def uniform_ring_matrix(self, radius, field):
+    def uniform_ring(self, radius, field):
         """
         Create the random uniform matrix such that each matrix values: -radius = |W_ij| = radius.
         :param radius: float, the radius of the matrix
@@ -57,7 +57,7 @@ class Reservoir:
 
         return W
 
-    def single_value_matrix(self, value):
+    def single_value(self, value):
         if isinstance(value, complex):
             value = torch.tensor(value, dtype=torch.complex64)
         else:
@@ -65,62 +65,4 @@ class Reservoir:
 
         W = value * torch.ones(self.d_out, self.d_in, dtype=value.dtype)
 
-        return W
-
-
-class StructuredReservoir(Reservoir):
-    def __init__(self, d_in, d_out):
-        """
-        Generate the discrete state matrix.
-        """
-
-        if d_in < 1:
-            raise ValueError("Input dimension must be an integer > 0.")
-
-        if d_out < d_in:
-            raise ValueError("Output dimension must be an integer >= input dimension.")
-
-        if d_in == 1:
-            warnings.warn("Input dimension is equal to 1: the structured reservoir is not effective.")
-        if d_out == d_in:
-            warnings.warn("Input dimension is equal to output dimension: the structured reservoir is not effective.")
-
-        super().__init__(d_in, d_out)
-
-        self.M = self._mask()
-
-    # TODO: repair mask dimension
-    def _mask(self):
-        if self.d_in == 1:
-            return torch.ones(self.d_out, self.d_in)
-
-        if self.d_out == self.d_in:
-            return torch.eye(self.d_in)
-
-        bits = [torch.tensor([0, 1]) for _ in range(self.d_in)]
-        all_combinations = torch.cartesian_prod(*bits)
-        non_zero_filter = all_combinations.sum(dim=1) != 0
-        mask = all_combinations[non_zero_filter]
-        num_repeats = (self.d_out + self.d_in - 1) // self.d_in
-
-        return mask.repeat(num_repeats, 1)[:self.d_out, :]
-
-    def uniform_disk_matrix(self, radius, field):
-        """
-        Create the random uniform matrix such that each matrix values: -radius <= |W_ij| < radius.
-        :param radius: float, the radius of the matrix
-        :param field: str, the field of the matrix, 'real' or 'complex'
-        """
-        W = super().uniform_disk_matrix(radius, field)
-        W = torch.einsum('ph,ph -> ph', self.M, W)
-        return W
-
-    def uniform_ring_matrix(self, radius, field):
-        W = super().uniform_ring_matrix(radius, field)
-        W = torch.einsum('ph,ph -> ph', self.M, W)
-        return W
-
-    def single_value_matrix(self, value):
-        W = super().single_value_matrix(value)
-        W = torch.einsum('ph,ph -> ph', self.M, W)
         return W
