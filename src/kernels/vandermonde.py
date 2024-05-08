f0e4734d52ba7a981b090c315df2c0feaddacc85
icezimmer <g.lombardi11@studenti.unipi.it>
2024-05-08 18:49:21 +0200

    fix the continuous dynamics

--- src/kernels/vandermonde.py
+++ src/kernels/vandermonde.py
@@ -47,30 +47,34 @@ class Vandermonde(nn.Module):
 
         if dt is None:
             state_reservoir = DiscreteStateReservoir(self.d_state)
-            Lambda_bar = state_reservoir.diagonal_state_space_matrix(
+            Lambda = state_reservoir.diagonal_state_space_matrix(
                 min_radius=strong_stability, max_radius=weak_stability, field=field)
-            B_bar = B
         elif dt > 0:
             state_reservoir = ContinuousStateReservoir(self.d_state)
             Lambda = state_reservoir.diagonal_state_space_matrix(
                 min_real_part=strong_stability, max_real_part=weak_stability, field=field)
-            Lambda_bar, B_bar = self._zoh(Lambda, B, dt)
         else:
             raise ValueError("Delta time dt must be positive: set dt > 0 or None for 'discrete dynamics'.")
 
+        self.dt = dt
+
         # Initialize parameters A, B and C
-        self.A = nn.Parameter(torch.view_as_real(Lambda_bar), requires_grad=True)  # (P, 2)
-        self.B = nn.Parameter(torch.view_as_real(B_bar), requires_grad=True)  # (P, H, 2)
+        self.A = nn.Parameter(torch.view_as_real(Lambda), requires_grad=True)  # (P, 2)
+        self.B = nn.Parameter(torch.view_as_real(B), requires_grad=True)  # (P, H, 2)
         self.C = nn.Parameter(torch.view_as_real(C), requires_grad=True)  # (H, P, 2)
 
-        if lr < 0.0 and wd < 0.0:
+        if lr < 0.0 or wd < 0.0:
             raise ValueError("Learning rate an weight decay for kernel parameters bust be positive.")
-        if lr > 0.001 and wd > 0.0:
+        if lr > 0.001 or wd > 0.0:
             warnings.warn("For a better optimization of the kernel parameters set lr <= 0.001 and wd = 0.0.")
 
         self.A._optim = {'lr': lr, 'weight_decay': wd}
         self.B._optim = {'lr': lr, 'weight_decay': wd}
         self.C._optim = {'lr': lr, 'weight_decay': wd}
+
+        # Register ones for zoh discretization
+        ones = torch.ones(size=Lambda.shape, dtype=torch.float32)
+        self.register_buffer('ones', ones)  # (L,)
 
         # Register powers for Vandermonde matrix
         powers = torch.arange(kernel_size, dtype=torch.float32)
@@ -93,25 +97,20 @@ class Vandermonde(nn.Module):
         else:
             raise ValueError(f"{param_name} is not a parameter in this module.")
 
-    @staticmethod
-    def _zoh(Lambda, B, dt):
-        """
-        Discretize the system using the zero-order-hold transform, where A = diag(Lambda):
-            A_bar = exp(A * dt)
-            B_bar = (A_bar - I) * inv(A) * B
-            C_bar = C
-            D_bar = D
-        :param Lambda: State Diagonal Matrix (Continuous System)
-        :param B: Input->State Matrix (Continuous System)
-        :param dt: Delta time for discretization
-        :return: Lambda_bar, B_bar (Discrete System)
-        """
-        Ones = torch.ones(size=Lambda.shape, dtype=torch.float32)
+    def _discretize_A(self):
+        A = torch.view_as_complex(self.A)
+        A_bar = torch.exp(A * self.dt)
 
-        Lambda_bar = torch.exp(Lambda * dt)
-        B_bar = torch.einsum('p,ph->ph', torch.mul(1 / Lambda, (Lambda_bar - Ones)), B)
+        return A_bar
 
-        return Lambda_bar, B_bar
+    def _discretize_AB(self):
+        A = torch.view_as_complex(self.A)
+        B = torch.view_as_complex(self.B)
+        A_bar = self._discretize_A()
+        B_bar = torch.einsum('p,ph->ph', torch.mul(1 / A, (A_bar - self.ones)), B)
+
+        return A_bar, B_bar
+
 
     def _construct_vandermonde(self):
         """
@@ -122,8 +121,12 @@ class Vandermonde(nn.Module):
             l = 0, ..., kernel_size-1.
         returns: V: (P,L)
         """
-        A = torch.view_as_complex(self.A)
-        V = A.unsqueeze(-1) ** self.powers  # (P, L)
+        if self.dt is None:
+            A_bar = torch.view_as_complex(self.A)
+        else:
+            A_bar = self._discretize_A()
+
+        V = A_bar.unsqueeze(-1) ** self.powers  # (P, L)
         return V
 
     def step(self, u, x=None):
@@ -136,13 +139,18 @@ class Vandermonde(nn.Module):
         :return: y: time step output of shape (B, H), x: time step state of shape (B, P)
         """
         u = u.to(dtype=torch.complex64)
-        A = torch.view_as_complex(self.A)  # (P)
-        B = torch.view_as_complex(self.B)  # (P, H)
+
+        if self.dt is None:
+            A_bar = torch.view_as_complex(self.A)
+            B_bar = torch.view_as_complex(self.B)
+        else:
+            A_bar, B_bar = self._discretize_AB()
+
         C = torch.view_as_complex(self.C)  # (H, P)
 
         if x is None:
             x = self.x0.unsqueeze(0).expand(u.shape[0], -1)
-        x = torch.einsum('p,bp->bp', A, x) + torch.einsum('ph,bh->bp', B, u)  # (B,P)
+        x = torch.einsum('p,bp->bp', A_bar, x) + torch.einsum('ph,bh->bp', B_bar, u)  # (B,P)
         y = torch.einsum('hp,bp->bh', C, x).real  # (B,H)
 
         return y, x
@@ -153,10 +161,15 @@ class Vandermonde(nn.Module):
         :return: kernel: 1d convolution kernel of shape (H, L)
         """
         V = self._construct_vandermonde()  # (P, L)
-        B = torch.view_as_complex(self.B)  # (P, H)
+
+        if self.dt is None:
+            B_bar = torch.view_as_complex(self.B)
+        else:
+            _, B_bar = self._discretize_AB()
+
         C = torch.view_as_complex(self.C)  # (H, P)
 
-        W = torch.einsum('hp,ph->hp', C, B)  # (H, P)
+        W = torch.einsum('hp,ph->hp', C, B_bar)  # (H, P)
         kernel = torch.einsum('hp,pl->hl', W, V)  # (H, L)
 
         return kernel, None
@@ -267,7 +280,13 @@ class VandermondeInputOutputReservoir(Vandermonde):
         self._freeze_parameter('B')  # (P, H, 2)
         self._freeze_parameter('C')  # (H, P, 2)
 
-        W = torch.einsum('hp,ph->hp', torch.view_as_complex(self.C), torch.view_as_complex(self.B))
+        C = torch.view_as_complex(self.C)  # (H, P)
+        if self.dt is None:
+            B_bar = torch.view_as_complex(self.B)
+        else:
+            _, B_bar = self._discretize_AB()
+
+        W = torch.einsum('hp,ph->hp', C, B_bar)  # (H, P)
         self.register_buffer('W', W)  # (H, P)
 
     def forward(self):
@@ -324,10 +343,14 @@ class VandermondeStateReservoir(Vandermonde):
         Generate the convolution kernel from the diagonal SSM parameters
         :return: kernel: 1d convolution kernel of shape (H, L)
         """
-        B = torch.view_as_complex(self.B)  # (P, H)
+        if self.dt is None:
+            B_bar = torch.view_as_complex(self.B)
+        else:
+            _, B_bar = self._discretize_AB()
+
         C = torch.view_as_complex(self.C)  # (H, P)
 
-        W = torch.einsum('hp,ph->hp', C, B)  # (H, P)
+        W = torch.einsum('hp,ph->hp', C, B_bar)  # (H, P)
         kernel = torch.einsum('hp,pl->hl', W, self.V)  # (H, L)
 
         return kernel, None
