8e2c5a298b929291b0afa3aac0fdfdd1a58d893f
icezimmer <g.lombardi11@studenti.unipi.it>
2024-05-08 18:49:00 +0200

    add the possibility of have more ssm so more A matrix

--- src/reservoir/state.py
+++ src/reservoir/state.py
@@ -21,7 +21,7 @@ class DiscreteStateReservoir:
         w_hh = (max_radius / torch.max(torch.abs(eigenvalues))) * w_hh
         return w_hh
 
-    def diagonal_state_space_matrix(self, min_radius, max_radius, field):
+    def diagonal_state_space_matrix(self, min_radius, max_radius, field, n_ssm=None):
         """
         Create a state matrix Lambda_bar for the discrete dynamics;
         lambda = radius * (cos(theta) + i * sin(theta)):
@@ -29,6 +29,9 @@ class DiscreteStateReservoir:
         theta in [0, 2pi).
         :return: Lambda_bar
         """
+        if n_ssm is not None and n_ssm <= 0:
+            raise ValueError("The input dimension must be a positive integer.")
+
         if min_radius > max_radius or min_radius < 0:
             raise ValueError("For the discrete dynamics we must have:"
                              "0 <= min_radius <= |lambda| < max_radius.")
@@ -37,25 +40,36 @@ class DiscreteStateReservoir:
                           "0 <= min_radius <= |lambda| < max_radius <= 1.")
 
         if field == 'complex':
+            if n_ssm is None:
+                size = (self.d_state,)
+            else:
+                size = (self.d_state, n_ssm)
             radius = (min_radius + (max_radius - min_radius) *
-                      torch.sqrt(torch.rand(self.d_state, dtype=torch.float32)))
-            theta = 2 * torch.pi * torch.rand(self.d_state, dtype=torch.float32)
+                      torch.sqrt(torch.rand(size=size, dtype=torch.float32)))
+            theta = 2 * torch.pi * torch.rand(size=size, dtype=torch.float32)
             alpha_tensor = radius * torch.cos(theta)
             omega_tensor = radius * torch.sin(theta)
         elif field == 'real':
-            half_d_state = self.d_state // 2
+            if n_ssm is None:
+                size = (self.d_state // 2,)
+            else:
+                size = (self.d_state // 2, n_ssm)
             radius = (min_radius + (max_radius - min_radius) *
-                      torch.sqrt(torch.rand(half_d_state, dtype=torch.float32)))
-            theta = torch.pi * torch.rand(half_d_state, dtype=torch.float32)
-            alpha_tensor = torch.cat((radius * torch.cos(theta), radius * torch.cos(theta)), 0)
-            omega_tensor = torch.cat((radius * torch.sin(theta), -radius * torch.sin(theta)), 0)
+                      torch.sqrt(torch.rand(size=size, dtype=torch.float32)))
+            theta = torch.pi * torch.rand(size=size, dtype=torch.float32)
+            alpha_tensor = torch.cat(tensors=(radius * torch.cos(theta), radius * torch.cos(theta)), dim=0)
+            omega_tensor = torch.cat(tensors=(radius * torch.sin(theta), -radius * torch.sin(theta)), dim=0)
             if self.d_state % 2 == 1:
+                if n_ssm is None:
+                    size = (1,)
+                else:
+                    size = (1, n_ssm)
                 extra_radius = (min_radius + (max_radius - min_radius) *
-                                torch.rand(1, dtype=torch.float32))
+                                torch.rand(size=size, dtype=torch.float32))
                 # Choose 0 or pi randomly for extra_theta
                 extra_theta = torch.randint(0, 2, (1,)) * torch.pi
-                alpha_tensor = torch.cat((alpha_tensor, extra_radius * torch.cos(extra_theta)), 0)
-                omega_tensor = torch.cat((omega_tensor, extra_radius * torch.sin(extra_theta)), 0)
+                alpha_tensor = torch.cat(tensors=(alpha_tensor, extra_radius * torch.cos(extra_theta)), dim=0)
+                omega_tensor = torch.cat(tensors=(omega_tensor, extra_radius * torch.sin(extra_theta)), dim=0)
         else:
             raise ValueError("The field must be 'complex' or 'real'.")
 
@@ -86,7 +100,7 @@ class ContinuousStateReservoir:
 
         return w_hh
 
-    def diagonal_state_space_matrix(self, min_real_part, max_real_part, field):
+    def diagonal_state_space_matrix(self, min_real_part, max_real_part, field, n_ssm=None):
         """
         Create a state matrix Lambda for the continuous dynamics;
         lambda = log(radius) + i * theta:
@@ -94,6 +108,9 @@ class ContinuousStateReservoir:
         Im(lambda) in [0, 2pi).
         :return: Lambda
         """
+        if n_ssm is not None and n_ssm <= 0:
+            raise ValueError("The input dimension must be a positive integer.")
+
         if min_real_part > max_real_part:
             raise ValueError("For the continuous dynamics we must have:"
                              "min_real_part <= Re(lambda) < max_real_part.")
@@ -102,22 +119,34 @@ class ContinuousStateReservoir:
                           "min_real_part <= Re(lambda) < max_real_part <= 0.")
 
         if field == 'complex':
+            if n_ssm is None:
+                size = (self.d_state,)
+            else:
+                size = (self.d_state, n_ssm)
             real_tensor = (min_real_part +
-                           (max_real_part - min_real_part) * torch.rand(self.d_state, dtype=torch.float32))
-            imag_tensor = 2 * torch.pi * torch.rand(self.d_state, dtype=torch.float32)
+                           (max_real_part - min_real_part) * torch.rand(size=size, dtype=torch.float32))
+            imag_tensor = 2 * torch.pi * torch.rand(size=size, dtype=torch.float32)
         elif field == 'real':
-            half_d_state = self.d_state // 2
+            if n_ssm is None:
+                size = (self.d_state // 2,)
+            else:
+                size = (self.d_state // 2, n_ssm)
             real_tensor = (min_real_part +
-                           (max_real_part - min_real_part) * torch.rand(half_d_state, dtype=torch.float32))
-            imag_tensor = torch.pi * torch.rand(half_d_state, dtype=torch.float32)
+                           (max_real_part - min_real_part) * torch.rand(size=size, dtype=torch.float32))
+            imag_tensor = torch.pi * torch.rand(size=size, dtype=torch.float32)
             real_tensor = torch.cat((real_tensor, real_tensor), 0)
             imag_tensor = torch.cat((imag_tensor, -imag_tensor), 0)
             if self.d_state % 2 == 1:
-                extra_real = min_real_part + (max_real_part - min_real_part) * torch.rand(1, dtype=torch.float32)
+                if n_ssm is None:
+                    size = (1,)
+                else:
+                    size = (1, n_ssm)
+                extra_real = (min_real_part + (max_real_part - min_real_part) *
+                              torch.rand(size=size, dtype=torch.float32))
                 # Choose 0 or pi randomly for extra_imag (extra_theta)
                 extra_imag = torch.randint(0, 2, (1,)) * torch.pi
-                real_tensor = torch.cat((real_tensor, extra_real), 0)
-                imag_tensor = torch.cat((imag_tensor, extra_imag), 0)
+                real_tensor = torch.cat(tensors=(real_tensor, extra_real), dim=0)
+                imag_tensor = torch.cat(tensors=(imag_tensor, extra_imag), dim=0)
         else:
             raise ValueError("The field must be 'complex' or 'real'.")
 
