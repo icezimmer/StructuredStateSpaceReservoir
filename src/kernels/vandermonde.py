4506d3710212126429903e4e7f5bdb647919d603
icezimmer <g.lombardi11@studenti.unipi.it>
2024-05-13 17:41:26 +0200

    move vandermonde reservoir class

--- src/kernels/vandermonde.py
+++ src/kernels/vandermonde.py
@@ -175,7 +175,7 @@ class Vandermonde(nn.Module):
         return kernel, None
 
 
-class VandermondeInputReservoir(Vandermonde):
+class VandermondeFreezeB(Vandermonde):
     """Generate convolution kernel from diagonal SSM parameters."""
 
     def __init__(self, d_input, d_state, kernel_size,
@@ -210,7 +210,7 @@ class VandermondeInputReservoir(Vandermonde):
         self._freeze_parameter('B')  # (P, H, 2)
 
 
-class VandermondeOutputReservoir(Vandermonde):
+class VandermondeFreezeC(Vandermonde):
     """Generate convolution kernel from diagonal SSM parameters."""
 
     def __init__(self, d_input, d_state, kernel_size,
@@ -245,7 +245,7 @@ class VandermondeOutputReservoir(Vandermonde):
         self._freeze_parameter('C')  # (H, P, 2)
 
 
-class VandermondeInputOutputReservoir(Vandermonde):
+class VandermondeFreezeBC(Vandermonde):
     """Generate convolution kernel from diagonal SSM parameters."""
 
     def __init__(self, d_input, d_state, kernel_size,
@@ -280,27 +280,8 @@ class VandermondeInputOutputReservoir(Vandermonde):
         self._freeze_parameter('B')  # (P, H, 2)
         self._freeze_parameter('C')  # (H, P, 2)
 
-        C = torch.view_as_complex(self.C)  # (H, P)
-        if self.dt is None:
-            B_bar = torch.view_as_complex(self.B)
-        else:
-            _, B_bar = self._discretize_AB()
 
-        W = torch.einsum('hp,ph->hp', C, B_bar)  # (H, P)
-        self.register_buffer('W', W)  # (H, P)
-
-    def forward(self):
-        """
-        Generate the convolution kernel from the diagonal SSM parameters
-        :return: kernel: 1d convolution kernel of shape (H, L)
-        """
-        V = self._construct_vandermonde()  # (P, L)
-        kernel = torch.einsum('hp,pl->hl', self.W, V)  # (H, L)
-
-        return kernel, None
-
-
-class VandermondeStateReservoir(Vandermonde):
+class VandermondeFreezeA(Vandermonde):
     """Generate convolution kernel from diagonal SSM parameters."""
 
     def __init__(self, d_input, d_state, kernel_size,
@@ -356,7 +337,7 @@ class VandermondeStateReservoir(Vandermonde):
         return kernel, None
 
 
-class VandermondeInputStateReservoir(VandermondeStateReservoir):
+class VandermondeFreezeAB(VandermondeFreezeA):
     """Generate convolution kernel from diagonal SSM parameters."""
 
     def __init__(self, d_input, d_state, kernel_size,
@@ -391,7 +372,7 @@ class VandermondeInputStateReservoir(VandermondeStateReservoir):
         self._freeze_parameter('B')  # (P, H, 2)
 
 
-class VandermondeStateOutputReservoir(VandermondeStateReservoir):
+class VandermondeFreezeAC(VandermondeFreezeA):
     """Generate convolution kernel from diagonal SSM parameters."""
 
     def __init__(self, d_input, d_state, kernel_size,
@@ -424,117 +405,3 @@ class VandermondeStateOutputReservoir(VandermondeStateReservoir):
                          field)
 
         self._freeze_parameter('C')  # (H, P, 2)
-
-
-class VandermondeReservoir(nn.Module):
-    """
-    Generate convolution kernel from diagonal SSM parameters
-    """
-
-    def __init__(self, d_input, d_state, kernel_size,
-                 dt, strong_stability, weak_stability,
-                 input2state_scaling=1.0,
-                 state2output_scaling=1.0,
-                 field='complex'):
-        """
-        Construct the convolution kernel.
-        Assuming diagonal state matrix A of shape (d_state), the Vandermonde Kernel is:
-            kernel[i,l] = C[i,:] * diag(A)^l * B[:,i] = (C[i,:] .* B[:,i]) * A^l
-        where:
-            diag(A)[j,j] = A[j]
-            A^l[j] = A[j]^l
-        and:
-            j = 0, ..., d_state-1
-            i = 0, ..., d_input-1
-            l = 0, ..., kernel_size-1.
-        :param d_input: dimensionality of the input space
-        :param d_state: dimensionality of the latent space
-        :param dt: delta time for continuous dynamics (default: None for discrete dynamics)
-        :param field: field for the state 'real' or 'complex' (default: 'complex')
-        """
-        super().__init__()
-
-        self.d_input = d_input
-        self.d_state = d_state
-        self.d_output = self.d_input  # Necessary condition for the Vandermonde kernel (SISO)
-
-        self.register_buffer('x0', torch.zeros(self.d_state, dtype=torch.complex64))
-
-        input2state_reservoir = ReservoirMatrix(d_in=self.d_input, d_out=self.d_state)
-        state2output_reservoir = ReservoirMatrix(d_in=self.d_state, d_out=self.d_output)
-
-        B = input2state_reservoir.uniform_disk(radius=input2state_scaling, field=field)
-        C = state2output_reservoir.uniform_disk(radius=state2output_scaling, field=field)
-
-        if dt is None:
-            state_reservoir = DiscreteStateReservoir(self.d_state)
-            Lambda_bar = state_reservoir.diagonal_state_space_matrix(
-                min_radius=strong_stability, max_radius=weak_stability, field=field)
-            B_bar = B
-        elif dt > 0:
-            state_reservoir = ContinuousStateReservoir(self.d_state)
-            Lambda = state_reservoir.diagonal_state_space_matrix(
-                min_real_part=strong_stability, max_real_part=weak_stability, field=field)
-            Lambda_bar, B_bar = self._zoh(Lambda, B, dt)
-        else:
-            raise ValueError("Delta time dt must be positive: set dt > 0 or None for 'discrete dynamics'.")
-
-        self.register_buffer('A', Lambda_bar)  # (P,)
-        self.register_buffer('B', B_bar)  # (P, H)
-        self.register_buffer('C', C)  # (H, P)
-
-        W = torch.einsum('hp,ph -> hp', self.C, self.B)
-
-        powers = torch.arange(kernel_size, dtype=torch.float32)
-        V = self.A.unsqueeze(-1) ** powers
-
-        kernel = torch.einsum('hp,pl->hl', W, V)  # (H, L)
-        self.register_buffer('K', kernel)  # (H, L)
-
-
-
-    @staticmethod
-    def _zoh(Lambda, B, dt):
-        """
-        Discretize the system using the zero-order-hold transform, where A = diag(Lambda):
-            A_bar = exp(A * dt)
-            B_bar = (A_bar - I) * inv(A) * B
-            C_bar = C
-            D_bar = D
-        :param Lambda: State Diagonal Matrix (Continuous System)
-        :param B: Input->State Matrix (Continuous System)
-        :param dt: Delta time for discretization
-        :return: Lambda_bar, B_bar (Discrete System)
-        """
-        Ones = torch.ones(size=Lambda.shape, dtype=torch.float32)
-
-        Lambda_bar = torch.exp(Lambda * dt)
-        B_bar = torch.einsum('p,ph->ph', torch.mul(1 / Lambda, (Lambda_bar - Ones)), B)
-
-        return Lambda_bar, B_bar
-
-    def step(self, u, x=None):
-        """
-        Step one time step as a recurrent model. Intended to be used during validation.
-            x_new = A * x_old + B * u_new
-            y_new = C * x_new
-        :param u: time step input of shape (B, H)
-        :param x: time step state of shape (B, P)
-        :return: y: time step output of shape (B, H), x: time step state of shape (B, P)
-        """
-        u = u.to(dtype=torch.complex64)
-        if x is None:
-            x = self.x0.unsqueeze(0).expand(u.shape[0], -1)
-        x = torch.einsum('p,bp->bp', self.A, x) + torch.einsum('ph,bh->bp', self.B, u)  # (B,P)
-        y = torch.einsum('hp,bp->bh', self.C, x).real  # (B,H)
-
-        return y, x
-
-    def forward(self):
-        """
-        Generate the convolution kernel from the diagonal SSM parameters
-        :return: kernel: 1d convolution kernel of shape (H, L)
-        """
-        kernel = self.K  # (H, L)
-
-        return kernel, None
