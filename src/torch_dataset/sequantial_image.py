4d15cf331043c33a34b6207c6ffee9f960a2c197
icezimmer <g.lombardi11@studenti.unipi.it>
2024-05-14 17:58:41 +0200

    added permutation parameter

--- src/torch_dataset/sequantial_image.py
+++ src/torch_dataset/sequantial_image.py
@@ -4,11 +4,13 @@ from torch.utils.data import Dataset
 
 class SequentialImage2Classify(Dataset):
 
-    def __init__(self, dataset):
+    def __init__(self, dataset, permutation=None):
         self.data = []
         for image, label in dataset:
             image = image.to(dtype=torch.float32)
             image = image.view(image.shape[0], -1)  # (C, H, W) -> (C, H * W)
+            if permutation is not None:
+                image = image[:, permutation]  # Apply the permutation
             label = torch.tensor(label, dtype=torch.long)
 
             self.data.append((image, label))
