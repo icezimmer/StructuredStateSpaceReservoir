fix esn preactivation computation

src/deep/stacked.py
@@ -136,8 +136,9 @@ class StackedEchoState(nn.Module):
 
         self.d_output = self.n_layers * self.d_state
 
-        self.layers = nn.ModuleList([ESN(d_input=d_input, d_state=self.d_state, **block_args)
-                                     for _ in range(self.n_layers)])
+        self.layers = nn.ModuleList([ESN(d_input=d_input, d_state=self.d_state, **block_args)] +
+                                    [ESN(d_input=self.d_state, d_state=self.d_state, **block_args)
+                                    for _ in range(self.n_layers - 1)])
 
         self.transient = transient
 
@@ -160,7 +161,7 @@ class StackedEchoState(nn.Module):
         """
         Forward method for the DeepESN model.
         :param  x: input sequence, torch tensor of shape (B, d_input, L)
-        :return: output sequence, torch tensor of shape (B, d_output, L - w)
+        :return: output sequence, torch tensor of shape (B, d_state, L - w)
         """
         x_list = []
         for layer in self.layers:

src/models/esn/esn.py
@@ -38,8 +38,8 @@ class ESN(nn.Module):
         """
         if x is None:
             x = self.x0.unsqueeze(0).expand(u.size(0), -1)
-        preactivation = (torch.einsum('ph, bh -> bp', self.w_in, u) +
-                         torch.einsum('pp, bp -> bp', self.w_hh, x) +
+        preactivation = (torch.matmul(u, self.w_in.t()) +
+                         torch.matmul(x, self.w_hh.t()) +
                          self.bias)
         x = (1 - self.leakage_rate) * x + self.leakage_rate * self.nl(preactivation)
         return x
@@ -48,7 +48,7 @@ class ESN(nn.Module):
         """
         Forward method for the ESN model
         :param u: batched input sequence of shape (B, H, L) = (batch_size, d_input, input_length)
-        :return: x: batched state sequence of shape (B, H, L) = (batch_size, d_state, input_length)
+        :return: x: batched state sequence of shape (B, P, L) = (batch_size, d_state, input_length)
         """
         x_prev = self.x0.unsqueeze(0).expand(u.shape[0], -1)
         x_list = []