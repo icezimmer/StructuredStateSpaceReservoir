28afc7bb52e5fefafd98b8af8814c7bd2ed4636d
icezimmer <g.lombardi11@studenti.unipi.it>
2024-07-30 21:18:10 +0200

    set postprocess and realfun to readout

--- src/models/rssm/rssm.py
+++ src/models/rssm/rssm.py
@@ -1,7 +1,6 @@
 import torch
 from src.models.rssm.convolutions.fft_reservoir import FFTConvReservoir
-from src.models.rssm.realfun_complexvar.complex_to_real import (ComplexToRealPart, ComplexToRealByGLU, ComplexToABS,
-                                                                ComplexToAngle)
+from src.models.rssm.realfun_complexvar.complex_to_real import Real, RealReLU, RealTanh, RealImagTanhGLU, ABSTanh, AngleTanh
 
 """
 see: https://github.com/i404788/s5-pytorch/tree/74e2fdae00b915a62c914bf3615c0b8a4279eb84
@@ -26,9 +25,9 @@ class RSSM(torch.nn.Module):
         if kernel not in kernel_classes:
             raise ValueError('Kernel must be one of {}'.format(kernel_classes))
 
-        realfuns = ['real', 'abs', 'angle', 'glu']
+        realfuns = ['real', 'real+relu', 'real+tanh', 'glu', 'abs+tanh', 'angle+tanh']
         if realfun not in realfuns:
-            raise ValueError('Mixing Layer must be one of {}'.format(realfuns))
+            raise ValueError('Real Function of Complex Vars must be one of {}'.format(realfuns))
 
         super().__init__()
 
@@ -37,14 +36,20 @@ class RSSM(torch.nn.Module):
         self.layer = FFTConvReservoir(d_input=self.d_model, d_state=self.d_model, kernel=kernel,
                                       **layer_args)
 
+        self.postprocess = RealReLU()
+
         if realfun == 'real':
-            self.realfun = ComplexToRealPart()
-        elif realfun == 'abs':
-            self.realfun = ComplexToABS()
-        elif realfun == 'angle':
-            self.realfun = ComplexToAngle()
+            self.realfun = Real()
+        elif realfun == 'real+relu':
+            self.realfun = RealReLU()
+        elif realfun == 'real+tanh':
+            self.realfun = RealTanh()
         elif realfun == 'glu':
-            self.realfun = ComplexToRealByGLU()
+            self.realfun = RealImagTanhGLU()
+        elif realfun == 'abs+tanh':
+            self.realfun = ABSTanh()
+        elif realfun == 'angle+tanh':
+            self.realfun = AngleTanh()
 
     # TODO: implement step method for mixing layer
     def step(self, u, x):
@@ -55,7 +60,7 @@ class RSSM(torch.nn.Module):
         :return: output step (B, H), new state (B, P)
         """
         y, x = self.layer.step(u, x)
-        y = self.realfun(y)
+        y = self.postprocess(y)
 
         return y, x
 
@@ -63,11 +68,15 @@ class RSSM(torch.nn.Module):
         """
         Forward method for the RSSM layer.
         :param u: batched input sequence of shape (B, H, L) = (batch_size, d_input, input_length)
-        :return: y: batched output sequence of shape (B, H, L) = (batch_size, d_output, input_length)
+        :return:
+            y: batched output sequence of shape (B, H, L) = (batch_size, d_output, input_length)
+                as inout of the next layer
+            z: batched output sequence of shape (B, H, L) = (batch_size, d_output, input_length)
+                to collect and train the readout
         """
-        y, _ = self.layer(u)
-        y = self.realfun(y)
-        x = y
+        u, _ = self.layer(u)
 
-        # Return a dummy state to satisfy this repo's interface, but this can be modified
-        return y, x
+        y = self.postprocess(u)
+        z = self.realfun(u)
+
+        return y, z
