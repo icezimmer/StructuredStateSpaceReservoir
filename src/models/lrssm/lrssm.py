01715f2f213d16c032912997609fbd20a10bf5f2
icezimmer <g.lombardi11@studenti.unipi.it>
2024-07-30 21:15:57 +0200

    set learning layer

--- src/models/lrssm/lrssm.py
+++ src/models/lrssm/lrssm.py
@@ -1,8 +1,7 @@
 import torch
 import torch.nn as nn
 from src.models.rssm.convolutions.fft_reservoir import FFTConvReservoir
-from src.models.lrssm.realfun_complexvar.complex_to_real import ComplexToReal
-from src.layers.online import ConvolutionSignal
+from src.models.lrssm.realfun_complexvar.complex_to_real import RealImag, Real
 
 """
 see: https://github.com/i404788/s5-pytorch/tree/74e2fdae00b915a62c914bf3615c0b8a4279eb84
@@ -12,6 +11,7 @@ see: https://github.com/i404788/s5-pytorch/tree/74e2fdae00b915a62c914bf3615c0b8a
 class LRSSM(torch.nn.Module):
     def __init__(self, d_model,
                  kernel,
+                 act,
                  dropout=0.0,
                  **layer_args):
         """
@@ -25,17 +25,28 @@ class LRSSM(torch.nn.Module):
         if kernel not in kernel_classes:
             raise ValueError('Kernel must be one of {}'.format(kernel_classes))
 
+        activations = ['relu', 'tanh', 'glu']
+        if act not in activations:
+            raise ValueError('Real Function of Complex Vars must be one of {}'.format(activations))
+
         super().__init__()
 
         self.d_model = d_model
 
-        self.layer = FFTConvReservoir(d_input=self.d_model, d_state=self.d_model, kernel=kernel,
+        self.layer = FFTConvReservoir(d_input=self.d_model, d_state=self.d_model, kernel=kernel, discrete=False,
                                       **layer_args)
-
-        self.realfun = ComplexToReal()
-
-        self.learning_layer = nn.Sequential(nn.Conv1d(in_channels=2 * d_model, out_channels=2 * d_model, kernel_size=1),
-                                            nn.GLU(dim=-2))
+        if act == 'glu':
+            self.learning_layer = nn.Sequential(RealImag(),
+                                                nn.Conv1d(in_channels=2 * d_model, out_channels=2 * d_model, kernel_size=1),
+                                                nn.GLU(dim=-2))
+        elif act == 'tanh':
+            self.learning_layer = nn.Sequential(Real(),
+                                                nn.Conv1d(in_channels=d_model, out_channels=d_model, kernel_size=1),
+                                                nn.Tanh())
+        elif act == 'relu':
+            self.learning_layer = nn.Sequential(Real(),
+                                                nn.Conv1d(in_channels=d_model, out_channels=d_model, kernel_size=1),
+                                                nn.ReLU())
 
         self.drop = nn.Dropout(dropout) if dropout > 0 else nn.Identity()
 
@@ -47,7 +58,6 @@ class LRSSM(torch.nn.Module):
         Returns: y (B, H), state (B, P)
         """
         y, x = self.layer.step(u, x)
-        y = self.realfun(y)
         y = self.learning_layer(y)
         y = self.drop(y)
 
@@ -60,7 +70,6 @@ class LRSSM(torch.nn.Module):
         :return: y: batched output sequence of shape (B,H,L) = (batch_size, d_output, input_length)
         """
         y, _ = self.layer(u)
-        y = self.realfun(y)
         y = self.learning_layer(y)
         y = self.drop(y)
 
