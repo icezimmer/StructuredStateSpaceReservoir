badad3e76db39e18cb24fdb5c2b744df93a55031
icezimmer <g.lombardi11@studenti.unipi.it>
2024-08-06 05:17:20 +0200

    set seed fo tf

--- src/utils/experiments.py
+++ src/utils/experiments.py
@@ -1,12 +1,14 @@
 import torch
 import random
 import numpy as np
+import tensorflow as tf
 
 
 def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    tf.random.set_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
