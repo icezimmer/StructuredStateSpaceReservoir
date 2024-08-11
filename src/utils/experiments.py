8bb49db775a1cb35f28bf178a81993aa04390025
icezimmer <g.lombardi11@studenti.unipi.it>
2024-08-11 02:10:20 +0200

    add read yaml

--- src/utils/experiments.py
+++ src/utils/experiments.py
@@ -2,6 +2,7 @@ import torch
 import random
 import numpy as np
 import tensorflow as tf
+import yaml
 
 
 def set_seed(seed):
@@ -14,3 +15,8 @@ def set_seed(seed):
         torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
+
+def read_yaml_to_dict(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
