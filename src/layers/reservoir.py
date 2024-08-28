09f30ff3a5913d82c4e7fafd0469601c90b5a8ba
icezimmer <g.lombardi11@studenti.unipi.it>
2024-08-28 20:16:09 +0200

    add length_last or not for forward

--- src/layers/reservoir.py
+++ src/layers/reservoir.py
@@ -7,13 +7,16 @@ class LinearReservoirRing(nn.Module):
     def __init__(self, d_input, d_output,
                  min_radius=0.0,
                  max_radius=1.0,
-                 field='real'):
+                 field='real',
+                 length_last=True):
         super().__init__()
 
         self.d_input = d_input
         self.d_output = d_output
 
         self.field = field
+
+        self.length_last = length_last
 
         reservoir = ReservoirMatrix(d_in=self.d_input, d_out=self.d_output)
         W_in = reservoir.uniform_ring(min_radius=min_radius, max_radius=max_radius, field=self.field)
@@ -25,7 +28,10 @@ class LinearReservoirRing(nn.Module):
         return u
 
     def forward(self, u):
-        u = torch.einsum('ph, bhl -> bpl', self.W_in, u)
+        if self.length_last:
+            u = torch.einsum('ph, bhl -> bpl', self.W_in, u)
+        else:
+            u = torch.einsum('ph, blh -> blp', self.W_in, u)
 
         return u
 
@@ -33,11 +39,14 @@ class LinearReservoirRing(nn.Module):
 class LinearReservoirInterval(nn.Module):
     def __init__(self, d_input, d_output,
                  min_value=0.0,
-                 max_value=1.0):
+                 max_value=1.0,
+                 length_last=True):
         super().__init__()
 
         self.d_input = d_input
         self.d_output = d_output
+
+        self.length_last = length_last
 
         reservoir = ReservoirMatrix(d_in=self.d_input, d_out=self.d_output)
         W_in = reservoir.uniform_interval(min_value=min_value, max_value=max_value)
@@ -49,6 +58,9 @@ class LinearReservoirInterval(nn.Module):
         return u
 
     def forward(self, u):
-        u = torch.einsum('ph, bhl -> bpl', self.W_in, u)
+        if self.length_last:
+            u = torch.einsum('ph, bhl -> bpl', self.W_in, u)
+        else:
+            u = torch.einsum('ph, blh -> blp', self.W_in, u)
 
         return u
