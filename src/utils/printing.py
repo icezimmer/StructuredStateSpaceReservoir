ffc72f9ff3ef9fbe639d5405f3bb7a32891bbd96
icezimmer <g.lombardi11@studenti.unipi.it>
2024-09-11 02:41:38 +0200

    from print to log

--- src/utils/printing.py
+++ src/utils/printing.py
@@ -1,18 +1,16 @@
+import logging
+
+
 def print_parameters(model):
     for name, param in model.named_parameters():
-        print('Parameter name:', name)
-        print(param.data.shape)
-        print('requires_grad:', param.requires_grad)
-        print('----------------------------------------------------')
+        logging.info(f'Parameter name: {name} = {param.data.shape}, requires_grad = {param.requires_grad}')
 
 
 def print_buffers(model):
     for name, buffer in model.named_buffers():
-        print('Buffer name:', name)
-        print(buffer.data.shape)
-        print('----------------------------------------------------')
+        logging.info(f'Buffer name: {name} = {buffer.data.shape}')
 
 
 def print_num_trainable_params(model):
     num_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print('Number of trainable parameters:', num_trainable_params)
+    logging.info(f'Number of trainable parameters: {num_trainable_params}')
